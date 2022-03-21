1beefaabcb0993913826cbdef2e6a792cb0943ad
diegosalazaar <diego_2000_28@hotmail.com>
2022-03-21 01:32:31 -0500

    add swiper component

--- src/views/Main/sections/FeaturedProducts/index.tsx
+++ src/views/Main/sections/FeaturedProducts/index.tsx
@@ -1,68 +1,11 @@
-import { Pagination, Navigation } from 'swiper'
-import { Swiper, SwiperSlide } from 'swiper/react'
-// Import Swiper styles
-import 'swiper/css'
-import 'swiper/css/pagination'
-import 'swiper/css/navigation'
+import { Swiper } from 'components'
 import './styles.scss'
 
 const FeaturedProducts = () => {
   return (
-    <Swiper
-      slidesPerView={4}
-      spaceBetween={0}
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-      onResize={(swiper) => {}}
-      onSwiper={(swiper) => {}}
-      className="mySwiperxDD "
-      modules={[Pagination, Navigation]}
-    >
-      {/* <div className="swiperContainerxD"> */}
-        <SwiperSlide className="swiperSlide">
-          <div className="featuredProduct">
-            <img src="https://i.ibb.co/4ZpMTrY/aceite-distro.png" alt="img" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiperSlide">
-          <div className="featuredProduct">
-            <img src="https://i.ibb.co/4ZpMTrY/aceite-distro.png" alt="img" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiperSlide">
-          <div className="featuredProduct">
-            <img src="https://i.ibb.co/4ZpMTrY/aceite-distro.png" alt="img" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiperSlide">
-          <div className="featuredProduct">
-            <img src="https://i.ibb.co/4ZpMTrY/aceite-distro.png" alt="img" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiperSlide">
-          <div className="featuredProduct">
-            <img src="https://i.ibb.co/4ZpMTrY/aceite-distro.png" alt="img" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiperSlide">
-          <div className="featuredProduct">
-            <img src="https://i.ibb.co/4ZpMTrY/aceite-distro.png" alt="img" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiperSlide">
-          <div className="featuredProduct">
-            <img src="https://i.ibb.co/4ZpMTrY/aceite-distro.png" alt="img" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiperSlide">
-          <div className="featuredProduct">
-            <img src="https://i.ibb.co/4ZpMTrY/aceite-distro.png" alt="img" />
-          </div>
-        </SwiperSlide>
-      {/* </div> */}
-    </Swiper>
+    <div className='featuredProductsContainer'>
+      <Swiper />
+    </div>
   )
 }
 
