5b288f4baf39614a8eb2b81fee7d4029e56a0e5a
diegosalazaar <diego_2000_28@hotmail.com>
2022-03-23 00:02:47 -0500

    add views structure

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,19 @@
-import { Main } from 'views'
+import { Contact, Main, Products } from 'views'
 
 export default [
   {
     path: '/',
     exact: true,
     component: <Main />,
+  },
+  {
+    path: '/products',
+    exact: true,
+    component: <Products />,
+  },
+  {
+    path: '/contact-us',
+    exact: true,
+    component: <Contact />,
   },
 ]
