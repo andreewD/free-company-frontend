06c35b6822602704ff62b4bf04f7894c4a098d6e
andr33wD <andree.anchi@chazki.com>
2022-04-10 19:08:05 -0500

    feat: Enterprise view added

---
+++ src/views/Services/sections/Home/index.tsx
@@ -0,0 +1,52 @@
+import { ArrowButton } from 'assets'
+import styled, { keyframes } from 'styled-components'
+import { ServicesImg } from 'assets'
+
+const flicker = keyframes`
+0% {
+  bottom: 1.75rem;
+}
+50% {
+  bottom: 1rem;
+}
+100% {
+  bottom: 1.75rem;
+}
+`
+const ArrowButtonContainer = styled('button')`
+  background: none;
+  border: none;
+  color: white;
+  position: absolute;
+  height: 12px;
+  left: 50%;
+  transform: translateX(-50%);
+  animation: ${flicker} 1.5s linear infinite;
+  cursor: pointer;
+`
+const CustomImg = styled('img')`
+  height: 100%;
+`
+
+const MainContainer = styled('section')`
+  height: 100vh;
+  width: 100%;
+  position: relative;
+  background-image: url(${ServicesImg});
+`
+
+function Home() {
+  const windowHeight = window.innerHeight
+  const goToScrooll = () => {
+    window.scrollTo(0, windowHeight)
+  }
+  return (
+    <MainContainer>
+      <ArrowButtonContainer onClick={goToScrooll}>
+        <CustomImg src={ArrowButton} alt="arrow" />
+      </ArrowButtonContainer>
+    </MainContainer>
+  )
+}
+
+export default Home
